ca155bc4a776fa975702ddeb80ee1e8a6f60608b
rsterenchak <rsterenchak@gmail.com>
2023-11-27 14:03:09 +0000

    added more icons

--- HomePage_main/src/index.js
+++ HomePage_main/src/index.js
@@ -2,7 +2,15 @@ import _ from 'lodash';
 import './style.css';
 import Icon1 from './github-mark.png';
 import Icon2 from './link-arrow.svg';
+import Portait1 from './pexels-alexandre.jpg'
 import printMe from './print.js';
+import Icon3 from './phone-outline.svg';
+import Icon4 from './email-outline.svg';
+
+
+/* var portrait1 = document.getElementsByClassName('picture');
+
+portrait1[0].src = Portait1;  */
 
 var element1 = document.getElementsByClassName('gitLink1');
 var element2 = document.getElementsByClassName('liveLink1');
@@ -41,4 +49,15 @@ var element12 = document.getElementsByClassName('liveLink6');
 element11[0].src = Icon1;
 element12[0].src = Icon2; 
 
+var element13 = document.getElementsByClassName('phoneIcon');
+
+element13[0].src = Icon3;
+
+var element14 = document.getElementsByClassName('mailIcon');
+
+element14[0].src = Icon4;
+
+var element15 = document.getElementsByClassName('mainGit');
+
+element15[0].src = Icon1;
   // document.body.appendChild(component());
